6711fe62de3698ba1ad00dd304c1065c93b8ee06
Dima <dima1999ostapenko@gmail.com>
2024-10-26 14:00:31 +0300

    add

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import s from "./HomePage.module.css";
 const HomePage = () => {
   return (
     <div className={s.homepage_wrapp}>
-      <h2 className={s.title}>Welcome to our application!</h2>
+      <h2 className={s.title}>Welcome to app!</h2>
       <p className={s.p}>
         This application allows you to store and use your contacts as you see
         fit.
