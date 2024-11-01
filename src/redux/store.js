fad98706633a49468e961e20a594c63293218c21
Dima <dima1999ostapenko@gmail.com>
2024-11-01 14:23:20 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "../redux/contacts/slice";
 import { filtersReducer } from "./filters/slice";
-import { authReducer } from "../redux/auth/slice";
+import authReducer from "../redux/auth/slice";
 import {
   persistStore,
   persistReducer,
