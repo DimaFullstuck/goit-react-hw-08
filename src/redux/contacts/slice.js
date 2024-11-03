5a96f5d09a247f595623c62126e930ef0f18e7f3
Dima <dima1999ostapenko@gmail.com>
2024-11-03 14:08:14 +0200

    add

--- src/redux/contacts/slice.js
+++ src/redux/contacts/slice.js
@@ -27,9 +27,7 @@ const contactsSlice = createSlice({
       .addCase(addContact.fulfilled, (state, action) => {
         state.items.push(action.payload);
       })
-      .addCase(logOut.fulfilled, (state) => {
-        state.items = [];
-      })
+
       .addCase(deleteContact.fulfilled, (state, action) => {
         state.items = state.items.filter(
           (contact) => contact.id !== action.payload.id
